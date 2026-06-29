d35530f75777d3f09726e3980eddf7922c3fb243
Abel Copa <abel.copa@dharbor.com>
2026-06-29 00:48:51 -0400

    chore(ecr): add ECR creation

---
+++ ecr.tf
@@ -0,0 +1,8 @@
+resource "aws_ecr_repository" "muyu" {
+  name                 =  var.name_prefix
+  image_tag_mutability = "IMMUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
